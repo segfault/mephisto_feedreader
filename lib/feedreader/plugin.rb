a3c16eab7383f16a25f58fc227fa7fff698e0eec
segfault <segfault@c10dc6e2-ae21-0410-890e-c4c894b30763>
2007-02-14 04:23:47 +0000

    plugin admin support
    git-svn-id: svn://hasno.info/mephisto/plugins/mephisto_feedreader@81 c10dc6e2-ae21-0410-890e-c4c894b30763

---
+++ lib/feedreader/plugin.rb
@@ -0,0 +1,29 @@
+# $Id$
+module MephistoPlugins
+  class Feedreader < MephistoPlugin
+    author 'Mark Guzman'
+    version '$Rev$'.gsub( "Rev: ", "" )
+    notes "Add liquid blocks displaying RSS feeds"
+    homepage "http://hasno.info/2006/11/11/mephisto-plugins"
+    
+    class Schema < ActiveRecord::Migration
+      def self.install
+        create_table :cached_feeds do |t|
+          t.column :href, :string
+          t.column :title, :string
+          t.column :link, :string
+          t.column :feed_data, :text
+          t.column :feed_data_type, :string
+          t.column :http_headers, :text
+          t.column :last_retrieved, :datetime
+          t.column :time_to_live, :integer
+          t.column :serialized, :text
+        end    
+      end
+
+      def self.uninstall
+        drop_table :cached_feeds
+      end
+    end
+  end
+end
