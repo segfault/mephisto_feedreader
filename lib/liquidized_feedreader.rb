fef6702c1ce82e4cfb445c260dabe39e2d7d4a23
Luigi Montanez <luigi.montanez@gmail.com>
2008-06-28 19:46:27 -0400

    Added new Liquid variables available from FeedTools

--- lib/liquidized_feedreader.rb
+++ lib/liquidized_feedreader.rb
@@ -1,11 +1,19 @@
 # $Id$
 # liquid-ify the feedtools feeditems
+#
+# Variables title through author_href added by Luigi Montanez
+
 class FeedTools::FeedItem
   def to_liquid
     {
       'href' => link,
       'description' => title,
       'extended' => content,
+      'title' => title,
+      'summary' => summary,
+      'author_name' => author.name,
+      'author_email' => author.email,
+      'author_href' => author.href,
       'tags' => tags,
       'time' => time
     }
